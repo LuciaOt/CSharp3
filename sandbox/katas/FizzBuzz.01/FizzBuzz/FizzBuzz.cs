e9e006d4d091c7a08902a4a8fe3d9169b6b162c1
Lucia Otiepkova <otiepkova.lucia@gmail.com>
2024-09-30 23:01:11 +0200

    FizzBuzz Solution + Extra

--- sandbox/katas/FizzBuzz.01/FizzBuzz/FizzBuzz.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/FizzBuzz.cs
@@ -1,31 +1,51 @@
-using System;
-
 public class FizzBuzz
 {
+    private void DisplayMessage(int number, bool isFizz, bool isBuzz)
+    {
+        if (isFizz && isBuzz)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (isFizz)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (isBuzz)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine(number);
+
+        }
+
+
+    }
+
     public void CountTo(int lastNumber)
     {
         for (int aktualniCislo = 1; aktualniCislo <= lastNumber; aktualniCislo++)
         {
-            if (aktualniCislo % 3 == 0 && aktualniCislo % 5 == 0)
-            {
-                Console.WriteLine("FizzBuzz");
-                //return;
-            }
-            else if (aktualniCislo % 3 == 0)
-            {
-                Console.WriteLine("Fizz");
-                //return;
-            }
-            else if (aktualniCislo % 5 == 0)
-            {
-                Console.WriteLine("Buzz");
-                //return;
-            }
-            else
-            {
-                Console.WriteLine(aktualniCislo);
-
-            }
+            bool isFizz = aktualniCislo % 3 == 0;
+            bool isBuzz = aktualniCislo % 5 == 0;
+            DisplayMessage(aktualniCislo, isFizz, isBuzz);
         }
+    }
+
+    public void ExtraMethod(int lastNumber)
+    {
+        for (int aktualniCislo = 1; aktualniCislo <= lastNumber; aktualniCislo++)
+        {
+            string aktualniCisloString = aktualniCislo.ToString();
+            bool containsThree = aktualniCisloString.Contains('3');
+            bool containsFive = aktualniCisloString.Contains('5');
+
+            DisplayMessage(aktualniCislo, containsThree, containsFive);
+
+
+        }
+
+
     }
 }
--- sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
+++ sandbox/katas/FizzBuzz.01/FizzBuzz/Program.cs
@@ -1,2 +1,7 @@
 ﻿FizzBuzz fizzBuzz = new FizzBuzz();
-fizzBuzz.CountTo(15);
+Console.WriteLine("First Solution:");
+fizzBuzz.CountTo(100);
+Console.WriteLine("------------------------------");
+Console.WriteLine("Extra Solution:");
+fizzBuzz.ExtraMethod(100);
+
