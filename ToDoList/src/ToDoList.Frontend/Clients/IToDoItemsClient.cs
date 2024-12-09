562fd96704d28ee6ec0cca8c2c5201c6bf9c5b82
Lucia Otiepkova <otiepkova.lucia@gmail.com>
2024-12-10 00:26:29 +0100

    added descriptions to submit button

--- ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
+++ ToDoList/src/ToDoList.Frontend/Clients/IToDoItemsClient.cs
@@ -6,9 +6,6 @@ public interface IToDoItemsClient
     public Task CreateItemAsync(ToDoItemView itemView);
     public Task<ToDoItemView?> ReadItemByIdAsync(int itemId);
     public Task<List<ToDoItemView>> ReadItemsAsync();
-
-    // Task DeleteItemAsync(int itemId); // Accepts only the ID
-
     public Task DeleteItemAsync(ToDoItemView itemView);
     public Task UpdateItemAsync(ToDoItemView itemView);
 }
